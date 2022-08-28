5219602ed09819ca9833cae1292e9c920fdba6fe
Jona <jonaeku@gmx.de>
2022-08-28 19:51:10 +0200

    Fix: backgroundcolor

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		// extend: {
+		extend: {
 			colors: {
 				'lol' : {
 					500: '#ff0000'
 				}
 			},
-		// },
+		},
 	},
 	plugins: [],
 }
