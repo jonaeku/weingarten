02d1a1eeeb978f3c9a430e83042961794f4f9f85
Jona <jonaeku@gmx.de>
2022-09-04 14:16:05 +0200

    Update: Layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,5 @@
 // /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'lol' : {
-					500: '#ff0000'
-				}
-			},
-		},
-	},
 	plugins: [],
 }
