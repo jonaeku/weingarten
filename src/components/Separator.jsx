8f68311b995b51eed076c4d1f4c7f28208fb5dc4
Jona <jonaeku@gmx.de>
2022-09-04 19:34:30 +0200

    Fix: Separator

--- src/components/Separator.jsx
+++ src/components/Separator.jsx
@@ -1,9 +1,9 @@
 const Separator = ({ color }) => {
     return (
         <div className="flex flex-row justify-center items-center h-14 my-8">
-            <hr className={`bg-[#565B60] black border-none h-0.5 w-10 rounded`} />
+            <hr className={`bg-[${color}] black border-none h-0.5 w-10 rounded`} />
             <img className="h-full px-4" src="/Sunflower.png" />
-            <hr className={`bg-[#565B60] black border-none h-0.5 w-10 rounded`} />
+            <hr className={`bg-[${color}] black border-none h-0.5 w-10 rounded`} />
             <hr />
         </div>
     );
