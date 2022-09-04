213985b040cad0af8125f95d46878ee17c4c2184
Jona <jonaeku@gmx.de>
2022-09-04 14:15:55 +0200

    Add: OpeningHours

---
+++ src/components/OpeningHours.jsx
@@ -0,0 +1,13 @@
+const OpeningHours = () => {
+    return (
+        <div className="flex flex-col items-center font-head bg-[#565B60] text-white py-6">
+            <h2 className="text-[30px]">Öffnungszeiten</h2><br />
+            <h3 className="text-[18px]">MO - DI - FR - SA</h3>
+            <h3 className="text-[30px]">17.30 - 22.00</h3><br />
+            <h3 className="text-[18px]">Sonntag</h3>
+            <h3 className="text-[30px]">12.30 - 22.00</h3>
+        </div>
+    );
+}
+
+export default OpeningHours;
