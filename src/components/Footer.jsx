8a3a470aafa99314ae7145ec742acf9c62e11660
Jona <jonaeku@gmx.de>
2022-09-04 18:51:58 +0200

    Add: LandingPage for Anlässe

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = ({ color }) => {
     return (
         <div className={`bg-[${color}] w-full flex flex-col lg:flex-row px-[12.5%] h-[95px] lg:h-[70px] justify-between items-center py-4 text-white font-head text-[12px] `}>
-            <a href="">Öffnungszeiten</a>
+            <a href="#Öffnungszeiten">Öffnungszeiten</a>
             <a href="">Impressum & Datenschutz</a>
             <a href="tel:063215763330">Telefon: 06321 5763330</a>
         </div>
--- src/components/Separator.jsx
+++ src/components/Separator.jsx
@@ -1,9 +1,9 @@
 const Separator = ({ color }) => {
     return (
         <div className="flex flex-row justify-center items-center h-14 my-8">
-            <hr class={`bg-[#565B60] black border-none h-0.5 w-10 rounded`} />
-            <img class="h-full px-4" src="/Sunflower.png" />
-            <hr class={`bg-[#565B60] black border-none h-0.5 w-10 rounded`} />
+            <hr className={`bg-[#565B60] black border-none h-0.5 w-10 rounded`} />
+            <img className="h-full px-4" src="/Sunflower.png" />
+            <hr className={`bg-[#565B60] black border-none h-0.5 w-10 rounded`} />
             <hr />
         </div>
     );
