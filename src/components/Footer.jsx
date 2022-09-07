2b5eecb9500959ffe9a47e8373ca41788f9e9a4e
Jona <jonaeku@gmx.de>
2022-09-07 18:47:26 +0200

    Trigger Footer rebuild

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = ({ color, textColor }) => {
     return (
-        <div className={`bg-[${color}] w-full flex flex-col lg:flex-row px-[12.5%] h-[95px] lg:h-[70px] justify-between items-center py-4 text-[${textColor}] font-head text-[12px] `}>
+        <div className={`bg-[${color}] w-full flex flex-col lg:flex-row px-[12.5%] h-[95px] lg:h-[70px] justify-between items-center py-4 text-[${textColor}] font-head text-[12px]`}>
             <a href="#Öffnungszeiten">Öffnungszeiten</a>
             <a href="">Impressum & Datenschutz</a>
             <a href="tel:063215763330">Telefon: 06321 5763330</a>
