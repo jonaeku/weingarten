580b4458c8cec8a826d9c2837fa67b32a9d49736
Jona <jonaeku@gmx.de>
2022-09-10 14:19:03 +0200

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
-const Footer = ({ color, textColor }) => {
+const Footer = ({ lightText }) => {
     return (
-        <div className={`bg-[${color}] w-full flex flex-col lg:flex-row px-[12.5%] h-[95px] lg:h-[70px] justify-between items-center py-4 text-[${textColor}] font-head text-[12px]`}>
+        <div className={`${lightText ? "bg-[#565B60] text-[white]" : "bg-[#E8E7E7] text-[black]"} w-full flex flex-col lg:flex-row px-[12.5%] h-[95px] lg:h-[70px] justify-between items-center py-4 font-head text-[12px]`}>
             <a href="/#Öffnungszeiten">Öffnungszeiten</a>
             <a href="">Impressum & Datenschutz</a>
             <a href="tel:063215763330">Telefon: 06321 5763330</a>
