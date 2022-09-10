4c82ba4ce57393ffee3ae5cf53c267b6d8afa4be
Jona <jonaeku@gmx.de>
2022-09-10 16:51:30 +0200

    Add: Impressum

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = ({ lightText }) => {
     return (
         <div className={`${lightText ? "bg-[#565B60] text-[white]" : "bg-[#E8E7E7] text-[black]"} w-full flex flex-col lg:flex-row px-[12.5%] h-[95px] lg:h-[70px] justify-between items-center py-4 font-head text-[12px]`}>
             <a href="/#Öffnungszeiten">Öffnungszeiten</a>
-            <a href="">Impressum & Datenschutz</a>
+            <a href="/Impressum">Impressum & Datenschutz</a>
             <a href="tel:063215763330">Telefon: 06321 5763330</a>
         </div>
     );
