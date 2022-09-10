21b2426e56d8a01cccbfc5414ca444de01cfc4d1
Jona <jonaeku@gmx.de>
2022-09-10 16:48:28 +0200

    Fix Seo issus

--- src/components/Separator.jsx
+++ src/components/Separator.jsx
@@ -2,7 +2,7 @@ const Separator = ({ color }) => {
     return (
         <div className="flex flex-row justify-center items-center h-14 my-8">
             <hr className={`bg-[${color}] black border-none h-0.5 w-10 rounded`} />
-            <img className="h-full px-4" src="/Sunflower.png" />
+            <img className="h-full px-4" src="/Sunflower.png" alt="SunflowerSeperator" />
             <hr className={`bg-[${color}] black border-none h-0.5 w-10 rounded`} />
             <hr />
         </div>
