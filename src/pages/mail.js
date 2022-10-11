62e611b2743e435e8701d3a8d61eca2e0fd1aa58
Jona <jonaeku@gmx.de>
2022-10-11 09:45:04 +0200

    Update mail adress

--- src/pages/mail.js
+++ src/pages/mail.js
@@ -4,7 +4,7 @@ sgMail.setApiKey(import.meta.env.PUBLIC_SENDGRID_API);
 export async function post({ request }) {
   const data = await request.json()
   await sgMail.send({
-    to: "hello@ionae.de",
+    to: "info@weingarten-maikammer.de",
     from: "Terminanfrage@ionae.de",
     templateId: "d-2ccd4d493c8d476b8aaeb104bc8dc3ae",
     dynamicTemplateData: {
